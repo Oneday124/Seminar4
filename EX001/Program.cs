42dd59ff6f32c3c4c13433b94c762292d0850388
Nikolay <Onedaydejavu124@mail.ru>
2022-09-11 23:05:25 +0700

    Написать программу замены элементов массива на противоположные

--- EX001/Program.cs
+++ EX001/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу замены элементов массива на противоположные
+void PrintArray(int[] array)
+{
+    foreach(var element in array)
+    Console.Write($"{element} ");
+}
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void Replacement(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+         array[i] = array[i] * -1;
+    }
+}
+Console.WriteLine("Введите длинну масива: ");
+int lenght = int.Parse(Console.ReadLine() ??"0");
+
+int[] array = new int[lenght];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+Replacement(array);
+PrintArray(array);
+
+
